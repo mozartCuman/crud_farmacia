fdf4ce4ecee43ff4375fb887d264aa5a0bbe8067
mozartCuman <mozartcumana@gmail.com>
2024-12-16 11:18:53 -0300

    app properties, join -> sgbd

---
+++ src/test/java/com/crud_farmacia/crud_farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_farmacia.crud_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
